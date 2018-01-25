72300cd6bb894da089767519a023fc3119b2d930
Harrison Welch <hwelch1@my.apsu.edu>
2018-01-25 10:18:58 -0600

    on click

--- app/src/main/java/com/example/nicholsonja/buttonsuccess/MainActivity.java
+++ app/src/main/java/com/example/nicholsonja/buttonsuccess/MainActivity.java
@@ -5,11 +5,12 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +27,8 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        findViewById(R.id.myButton).setOnClickListener(this);
     }
 
     @Override
@@ -48,5 +51,10 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onClick(View v) {
+        Log.i("MAIN","Hola Mundo");
     }
 }
